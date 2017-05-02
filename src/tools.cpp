11c3e94521780b6d4af9cf890c26ab22cf386eb3
Arkajyoti Misra <arkajyoti@gmail.com>
2017-05-02 06:13:08 -0500

    Formatting updated * file format updated to have consistent indent and braces * ProcessMeasurement is partially written

--- src/tools.cpp
+++ src/tools.cpp
@@ -9,6 +9,22 @@ Tools::Tools() {}
 
 Tools::~Tools() {}
 
+VectorXd Tools::PolarToCart(const VectorXd& polar)
+{
+    VectorXd cartesian(4);
+    float rho = polar(0);
+    float phi = polar(1);
+    float rho_dot = polar(2);
+
+    cartesian << rho * cos(phi), 
+                 rho * sin(phi), 
+                 rho_dot * rho_dot,
+                 phi,
+                 0.0;
+
+    return cartesian;
+}
+
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
